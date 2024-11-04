fbdeaa65737021ea7df0522502fe4434a5b5d431
Le Thanh Nguyen <u7594144@anu.edu.au>
2024-11-04 22:26:21 +1100

    fixed toggle button

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav');
 
 menuToggle.addEventListener('click', () => {
-nav.classList.toggle('active');
-menuToggle.classList.toggle('active');
+    nav.classList.toggle('active');
+    menuToggle.classList.toggle('active');
 });
 
 window.addEventListener('resize', () => {
