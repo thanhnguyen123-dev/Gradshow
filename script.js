6147e4269d495956c48009fb8a63f702f8847820
Le Thanh Nguyen <u7594144@anu.edu.au>
2024-11-05 17:16:32 +1100

    added cmts

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav');
 const gradShowText = document.querySelector('.gradshow-text');
 
+/* Menu toggle */
 menuToggle.addEventListener('click', () => {
+    // Toggle the active class on the nav and menu toggle
     nav.classList.toggle('active');
     menuToggle.classList.toggle('active');
 });
 
-
+/* Resize event */
 window.addEventListener('resize', () => {
+    // If the window is wider than 768px, remove the active class from the nav and menu toggle
     if (window.innerWidth > 768) {
         nav.classList.remove('active');
         menuToggle.classList.remove('active');
