fce95913cbe522bf06a46b8347391dd6557b28e1
Le Thanh Nguyen <u7594144@anu.edu.au>
2024-11-05 16:00:22 +1100

    more fixed

--- script.js
+++ script.js
@@ -1,14 +1,35 @@
 const menuToggle = document.querySelector('.menu-toggle');
 const nav = document.querySelector('nav');
+const gradShowText = document.querySelector('.gradshow-text');
 
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('active');
     menuToggle.classList.toggle('active');
 });
 
-window.addEventListener('resize', () => {
-if (window.innerWidth > 768) {
-    nav.classList.remove('active');
-    menuToggle.classList.remove('active');
+
+const updateText = () => {
+    if (gradShowText != null) {
+        if (window.innerWidth <= 1140) {
+            gradShowText.textContent = "GS2024";
+        }
+        else {
+            gradShowText.textContent = "GRADSHOW 2024";
+        }
+    }
 }
+
+window.addEventListener('resize', () => {
+    updateText();
+    if (window.innerWidth > 768) {
+        nav.classList.remove('active');
+        menuToggle.classList.remove('active');
+    }
+});
+
+window.addEventListener('DOMContentLoaded', () => {
+    updateText();
+    requestAnimationFrame(() => {
+        document.documentElement.style.visibility = 'visible';
+    });
 });
